1e1ebe18f47901ec6f6ee32cd6291185f9e3fcb2
Jude Hall <jude.rendel.hall@gmail.com>
2022-10-13 22:20:26 -0600

    Intial Commit

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const cors = require("cors")
+const Sequelize = require("sequelize")
+
+//Imported while following a MERN guide- cannot uninstall
+//const mongoose = require("mongoose")
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+require("dotenv").config()
+const port = process.env.PORT
+const sequelize = new Sequelize(process.env.CONNECTION_STRING, {
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            rejectUnathorized: false
+        }
+    }
+})
+
+app.listen(port, () => {
+    console.log('Server is running on port ${port}')
+})
